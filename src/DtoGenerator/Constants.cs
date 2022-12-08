9c581e822c3119fa7dc31f12740155c2d8fe45a6
David <justin@justinwritescode.com>
2022-12-08 00:41:32 -0500

    Checking in so I can banch

---
+++ src/DtoGenerator/Constants.cs
@@ -0,0 +1,19 @@
+/* 
+ * Constants.cs
+ * 
+ *   Created: 2022-12-06-08:59:23
+ *   Modified: 2022-12-06-08:59:24
+ * 
+ *   Author: Justin Chase <justin@justinwritescode.com>
+ *   
+ *   Copyright © 2022 Justin Chase, All Rights Reserved
+ *      License: MIT (https://opensource.org/licenses/MIT)
+ */ 
+
+namespace JustinWritesCode.CodeGeneration.DtoGenerator;
+
+public static class Constants
+{
+    public const string GenerateInsertDtoAttributeMetadataName = "GenerateInsertDtoAttribute";
+    public const string GenerateUpdateDtoAttributeMetadataName = "GenerateUpdateDtoAttribute";
+}
--- src/Extensions/StringExtensions.cs
+++ src/Extensions/StringExtensions.cs
@@ -10,18 +10,6 @@
  *      License: MIT (https://opensource.org/licenses/MIT)
  */
 
-//
-// StringExtensions.cs
-//
-//   Created: 2022-10-30-10:35:49
-//   Modified: 2022-10-30-10:40:41
-//
-//   Author: Justin Chase <justin@justinwritescode.com>
-//
-//   Copyright © 2022 Justin Chase, All Rights Reserved
-//      License: MIT (https://opensource.org/licenses/MIT)
-//
-
 using System.Diagnostics.CodeAnalysis;
 using System.Text.RegularExpressions;
 

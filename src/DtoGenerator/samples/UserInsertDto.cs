1dc202cd30136ee8f50c7fdb2841a90112f0cb06
Justin <justin@justinwritescode.com>
2023-01-08 22:08:34 -0600

    Update UserInsertDto.cs

---
+++ src/DtoGenerator/samples/UserInsertDto.cs
@@ -0,0 +1,12 @@
+/* 
+ * UserInsertDto.cs
+ * 
+ *   Created: 2023-01-06-01:30:03
+ *   Modified: 2023-01-06-01:30:04
+ * 
+ *   Author: Justin Chase <justin@justinwritescode.com>
+ *   
+ *   Copyright © 2022-2023 Justin Chase, All Rights Reserved
+ *      License: MIT (https://opensource.org/licenses/MIT)
+ */ 
+
